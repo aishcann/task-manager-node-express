d4bf1ce147cd4848b68be58e8d295b9f084b9950
aishcann <aish.cann@gmail.com>
2022-08-03 20:04:16 -0400

    initial setup of task routes and controller

--- app.js
+++ app.js
@@ -2,11 +2,18 @@
 const express = require('express')
 //initialize express
 const app = express()
+const tasks = require('./routes/tasks')
+
+//middleware
+
+app.use(express.json())
 
 // routes
 app.get('/hello', (req, res) => {
     res.send('Task Manager App')
-})
+}) 
+
+app.use('/api/v1/tasks', tasks)
 
 const port = 3000
 
